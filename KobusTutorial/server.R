975ff8051a1a09aac0580a64039f0fbd492137dc
Kobus Bosman <Kobus@Kobuss-MacBook-Pro.local>
2021-02-15 11:39:08 +0100

    Interactivity and observations

--- KobusTutorial/server.R
+++ KobusTutorial/server.R
@@ -1,15 +1,38 @@
-# server.R
 library(shiny)
+library(tidyverse)
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-  
+
+  # Observations
+  observeEvent(input$clicks, {});
+
+  # Data
+  data <- faithful[,2][order(faithful[,2])];
+
+  # Reactives
+  bins <- reactive({
+    seq(min(data), max(data), length.out = input$bins + 1)
+  });
+  histdata <- reactive({
+    hist(x = data, breaks = bins(), col = 'darkgray', border = 'white', main = isolate(input$title))
+  })
+
+  # Plotting
+  output$clicks <- renderPrint({
+    input$clicks[1];
+  })
+
+  output$ggplothist <- renderPlot({
+    ggplot(tibble(data), aes(data)) +
+      geom_histogram(breaks=bins()) +
+      ggtitle(isolate(input$title));
+  })
+
   output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    histdata()
+  })
+
+  output$stats <- renderPrint({
+    histdata()$counts;
   })
 }
--- KobusTutorial/ui.R
+++ KobusTutorial/ui.R
@@ -1,25 +1,29 @@
 # ui.r
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
   
-  # Application title
   titlePanel("Old Faithful Geyser Data"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
       sliderInput("bins",
                   "Number of bins:",
                   min = 1,
                   max = 50,
-                  value = 30)
+                  value = 30),
+      textInput(inputId = "title",
+                label = "Write a title",
+                value = "Histogram of Random Normal Values"),
+      actionButton(inputId = "clicks",
+                   label = "Click me")
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      verbatimTextOutput("clicks"),
+      plotOutput("distPlot"),
+      plotOutput("ggplothist"),
+      verbatimTextOutput("stats")
     )
   )
 )
